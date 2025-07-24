3b7ad338cc1fa7fb29a0ddb99da2ca6f28891514
sannya07 <sanyakansal07@gmail.com>
2025-07-24 15:39:39 +0530

    added middleware to protect routes

--- backend/routes/airoute.js
+++ backend/routes/airoute.js
@@ -1,28 +1,11 @@
 import express from "express";
 import { mcqget } from "../controllers/aiController.js";
-
+import { authenticateToken } from "../middleware/protect.js";
 import jwt from 'jsonwebtoken';
-
-export const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = req.cookies.token || (authHeader && authHeader.split(' ')[1]);
-
-  if (!token) {
-    return res.status(401).json({ error: "Access denied. No token provided." });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(403).json({ error: "Invalid or expired token" });
-  }
-};
 
 const router=express.Router();
 
-router.get('/mcq',mcqget);
-// router.get('/mcq',authenticateToken,mcqget);
+// router.get('/mcq',mcqget);
+router.get('/mcq',authenticateToken,mcqget);
 
 export default router;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,23 +1,7 @@
 import express from "express";
 import {register,login, verifyOtp} from "../controllers/authController.js";
 import jwt from 'jsonwebtoken';
-
-export const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = req.cookies.token || (authHeader && authHeader.split(' ')[1]);
-
-  if (!token) {
-    return res.status(401).json({ error: "Access denied. No token provided." });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(403).json({ error: "Invalid or expired token" });
-  }
-};
+import { authenticateToken } from "../middleware/protect.js";
 
 
 const router=express.Router();
