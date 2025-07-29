f3263af27c7c5388fb8c23bcc362112e89b0c724
sannya07 <sanyakansal07@gmail.com>
2025-07-29 23:48:46 +0530

    /

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "https://iterview-genie.vercel.app/", // Reflects the request origin
+  origin: "https://iterview-genie.vercel.app", // Reflects the request origin
   credentials: true,
 }));
 
